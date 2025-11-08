0ef8251888ccc854c7d59509b66375791e909119
jacksonlmr <38480226+jacksonlmr@users.noreply.github.com>
2025-11-08 15:43:42 -0800

    Part 1.b done. I think the values look correct now

--- experiment_1.py
+++ experiment_1.py
@@ -1,14 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-from helpers import magnitude, phase, center_spectrum
+from helpers import magnitude, phase, center_spectrum, create_figure
 
 outfile_path = "Experiment_1"
 #Part 1.a
-f = [2, 3, 4, 4] 
+f = np.array([2, 3, 4, 4]) 
+f_x_values = np.array([0, 1, 2, 3])
+
+# f = center_spectrum(f)
+# print(f"f centered: {f}")
 
 f_transformed = np.fft.fft(f, norm='forward')
 f_reverse_transform = np.fft.ifft(f_transformed, norm='forward')
+print(f"f transformed: {f_transformed}\n")
+
+f_mag = magnitude(f_transformed)
+f_phase = phase(f_transformed)
+
+print(f"f mag: {f_mag}\n")
+print(f"f phase: {f_phase}\n")
+
+create_figure('$f_mag', f_x_values, f_mag, outfile_path, True)
 
 #Part 1.b
 cos_sample_size = 128
@@ -20,112 +33,92 @@ period = (2*math.pi)/(2*math.pi*u) # = 1/u, period is x distance required to com
 max_x = period*cycles
 
 #calculate sample points within interval
-sample_points = np.linspace(0, max_x, 128)
-
+sample_points = np.linspace(0, max_x, 128, endpoint=False)
+print(len(sample_points))
 #sample cos function
 cos_samples = []
 for x in sample_points:
-    cos_samples.append((math.cos(2*math.pi*u*x))*(-1**x))#multiply by (-1^x) to center fourier transform
+    cos_samples.append((math.cos(2*math.pi*u*x)))
 
 cos_samples = np.array(cos_samples)
 
-#plot figure
-plt.figure(figsize=(10, 3))
-plt.plot(sample_points, cos_samples, 'o')
-plt.title("Sampled Cosine")
-plt.savefig(f"{outfile_path}\Sampled_Cosine.png")
+#compute DFT of cos function using f[x](-1)^x
+cos_samples = center_spectrum(cos_samples)
 
-#compute DFT of cos function
 cos_dft = np.fft.fft(cos_samples, norm='forward')
-u_sample_step = 1/(cos_samples[1]-cos_samples[0])
-cos_dft_freq = np.fft.fftfreq(cos_dft.size, d=u_sample_step)
 
-#plot real
-plt.figure(figsize=(10, 3))
-plt.plot(cos_dft_freq, cos_dft.real)
-plt.title("Cosine DFT Real")
-plt.savefig(f"{outfile_path}\Cosine_DFT_Real.png")
-
-#plot imaginary
-plt.figure(figsize=(10, 3))
-plt.plot(cos_dft_freq, cos_dft.imag)
-plt.title("Cosine DFT Imaginary")
-plt.savefig(f"{outfile_path}\Cosine_DFT_Imaginary.png")
+# u_sample_step = 1/(sample_points[1]-sample_points[0])
+cos_dft_freq = np.fft.fftfreq(cos_dft.size)
 
 #calculate magnitude and phase of cos dft
-cos_mag = []
-cos_phase = []
+cos_mag = magnitude(cos_dft)
+cos_phase = phase(cos_dft)
 
-for real, imag in zip(cos_dft.real, cos_dft.imag):
-    mag = math.sqrt(real**2+imag**2)
-    phase = math.atan(imag/real)
+#plot cosine samples
+create_figure('Sampled_Cosine', sample_points, cos_samples, outfile_path, True)
 
-    cos_mag.append(mag)
-    cos_phase.append(phase)
+#plot real
+create_figure('Cosine_DFT_Real', cos_dft_freq, cos_dft.real, outfile_path, False)
 
-#plot mag
-plt.figure(figsize=(10, 3))
-plt.plot(cos_dft_freq, cos_mag)
-plt.title("Magnitude of Cosine DFT")
-plt.savefig(f"{outfile_path}\Cosine_DFT_Mag.png")
-
-#plot phase
-plt.figure(figsize=(10, 3))
-plt.plot(cos_dft_freq, cos_phase)
-plt.title("Phase of Cosine DFT")
-plt.savefig(f"{outfile_path}\Cosine_DFT_Phase.png")
-
-
-#Part 1.c
-with open("Rect_128.txt") as file:
-    rect_values = file.readlines()
-    rect_values = [float(value.strip()) for value in rect_values]
-    rect_values = [(-1**value)*value for value in rect_values]#center fourier transform
-
-#calculate x-values
-#rectangle centered on x = 0
-rect_x_values = list(range(-len(rect_values)//2, len(rect_values)//2))
-
-#plot rectangle
-plt.figure(figsize=(10, 3))
-plt.plot(rect_x_values, rect_values, 'o')
-plt.title("Rectangle")
-plt.savefig(f"{outfile_path}\Rectangle.png")
-
-#compute dft of rectangle
-rect_dft = np.fft.fft(rect_values, norm='forward')
-
-#plot rect dft real
-plt.figure(figsize=(10, 3))
-plt.plot(rect_x_values, rect_dft.real)
-plt.title("Rectangle DFT Real")
-plt.savefig(f"{outfile_path}\Rectangle_DFT_Real.png")
-
-#plot rect dft imag
-plt.figure(figsize=(10, 3))
-plt.plot(rect_x_values, rect_dft.imag)
-plt.title("Rectangle DFT Imaginary")
-plt.savefig(f"{outfile_path}\Rectangle_DFT_Imag.png")
-
-#calculate magnitude and phase of rectangle dft
-rect_mag = []
-rect_phase = []
-
-for real, imag in zip(rect_dft.real, rect_dft.imag):
-    mag = math.sqrt(real**2+imag**2)
-    phase = math.atan(imag/real)
-
-    rect_mag.append(mag)
-    rect_phase.append(phase)
+#plot imaginary
+create_figure('Cosine_DFT_Imaginary', cos_dft_freq, cos_dft.imag, outfile_path, False)
 
 #plot mag
-plt.figure(figsize=(10, 3))
-plt.plot(rect_x_values, rect_mag)
-plt.title("Magnitude of Rectangle DFT")
-plt.savefig(f"{outfile_path}\Rectangle_DFT_Mag.png")
+create_figure('Cosine_DFT_Mag', cos_dft_freq, cos_mag, outfile_path, False)
 
 #plot phase
-plt.figure(figsize=(10, 3))
-plt.plot(rect_x_values, rect_phase)
-plt.title("Phase of Rectangle DFT")
-plt.savefig(f"{outfile_path}\Rectangle_DFT_Phase.png")
+create_figure('Cosine_DFT_Phase', cos_dft_freq, cos_phase, outfile_path, False)
+
+# #Part 1.c
+# with open("Rect_128.txt") as file:
+#     rect_values = file.readlines()
+#     rect_values = [float(value.strip()) for value in rect_values]
+#     rect_values = [(-1**value)*value for value in rect_values]#center fourier transform
+
+# #calculate x-values
+# #rectangle centered on x = 0
+# rect_x_values = list(range(-len(rect_values)//2, len(rect_values)//2))
+
+# #plot rectangle
+# plt.figure(figsize=(10, 3))
+# plt.plot(rect_x_values, rect_values, 'o')
+# plt.title("Rectangle")
+# plt.savefig(f"{outfile_path}\Rectangle.png")
+
+# #compute dft of rectangle
+# rect_dft = np.fft.fft(rect_values, norm='forward')
+
+# #plot rect dft real
+# plt.figure(figsize=(10, 3))
+# plt.plot(rect_x_values, rect_dft.real)
+# plt.title("Rectangle DFT Real")
+# plt.savefig(f"{outfile_path}\Rectangle_DFT_Real.png")
+
+# #plot rect dft imag
+# plt.figure(figsize=(10, 3))
+# plt.plot(rect_x_values, rect_dft.imag)
+# plt.title("Rectangle DFT Imaginary")
+# plt.savefig(f"{outfile_path}\Rectangle_DFT_Imag.png")
+
+# #calculate magnitude and phase of rectangle dft
+# rect_mag = []
+# rect_phase = []
+
+# for real, imag in zip(rect_dft.real, rect_dft.imag):
+#     mag = math.sqrt(real**2+imag**2)
+#     phase = math.atan(imag/real)
+
+#     rect_mag.append(mag)
+#     rect_phase.append(phase)
+
+# #plot mag
+# plt.figure(figsize=(10, 3))
+# plt.plot(rect_x_values, rect_mag)
+# plt.title("Magnitude of Rectangle DFT")
+# plt.savefig(f"{outfile_path}\Rectangle_DFT_Mag.png")
+
+# #plot phase
+# plt.figure(figsize=(10, 3))
+# plt.plot(rect_x_values, rect_phase)
+# plt.title("Phase of Rectangle DFT")
+# plt.savefig(f"{outfile_path}\Rectangle_DFT_Phase.png")
--- helpers.py
+++ helpers.py
@@ -1,21 +1,35 @@
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
-def magnitude(dft):
+def magnitude(dft: np.ndarray):
     mag_list = []
-    for real, imag in zip(dft.real, dft.imag):
+    for x in range(dft.size):
+        real = dft[x].real
+        imag = dft[x].imag
         mag = math.sqrt(real**2+imag**2)
         mag_list.append(mag)
 
-    return mag_list
+    return np.array(mag_list)
 
-def phase(dft):
+def phase(dft: np.ndarray):
     phase_list = []
-    for real, imag in zip(dft.real, dft.imag):
-        phase = math.atan(imag/real)
+    for x in range(dft.size):
+        real = dft[x].real
+        imag = dft[x].imag
+        phase = math.atan2(imag, real)
         phase_list.append(phase)
 
-    return phase_list
+    return np.array(phase_list)
+
+
+# def phase(dft):
+#     phase_list = []
+#     for real, imag in zip(dft.real, dft.imag):
+#         phase = math.atan(imag/real)
+#         phase_list.append(phase)
+
+#     return phase_list
 
 def center_spectrum(signal: np.ndarray):
     centered_signal = np.empty_like(signal)
@@ -23,3 +37,15 @@ def center_spectrum(signal: np.ndarray):
         centered_signal[x] = ((-1)**x)*signal[x]
 
     return centered_signal
+
+def create_figure(name: str, x: np.ndarray, y: np.ndarray, outfile_path: str, o: bool):
+    if o:
+        plt.figure(figsize=(10, 3))
+        plt.plot(x, y, 'o')
+        plt.title(name)
+        plt.savefig(f"{outfile_path}\{name}.png")
+    else:
+        plt.figure(figsize=(10, 3))
+        plt.plot(x, y)
+        plt.title(name)
+        plt.savefig(f"{outfile_path}\{name}.png")
